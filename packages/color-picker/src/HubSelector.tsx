aacf570069a3ee044d2c79479d3497d1e0e5e2a2
lingj <lingj.jhf@outlook.com>
2023-02-14 13:13:19 +0800

    重构组件

--- packages/color-picker/src/HubSelector.tsx
+++ packages/color-picker/src/HubSelector.tsx
@@ -1,101 +1,70 @@
 import Color from 'color'
-import { GDraggable } from '@lingjhf/draggable'
 import { Position, Size } from '@lingjhf/utils'
-import { createEffect, createSignal } from 'solid-js'
+import { createEffect, createSignal, mergeProps } from 'solid-js'
 import { xTransformHue, hueTransformX } from './utils'
+import { GRail } from './Rail'
 
 interface Props {
+  vertical: boolean
   color: Color
+  size: Size
   sliderSize: Size
   onChange: (color: Color) => void
 }
 
-//滑块默认大小
-const defaultSliderSize = { width: 12, height: 12 }
-
 export const GHubSelector = (props: Partial<Props>) => {
-  let hueSelectorRef: HTMLElement
-  const setHueSelectorRef = (el: HTMLElement) => (hueSelectorRef = el)
-
-  const [color, setColor] = createSignal(Color())
-  const [sliderSize, setSliderSize] = createSignal({
-    width: defaultSliderSize.width,
-    height: defaultSliderSize.height,
-  })
-  const [sliderX, setSliderX] = createSignal(0)
-  const [sliderMaxX, setSliderMaxX] = createSignal(0)
-  createEffect(() => {
-    let c: Color
-    if (props.color) {
-      c = props.color.hsv()
-    } else {
-      c = Color().hsv()
-    }
-    //根据颜色设置滑块位置
-    setSliderX(hueTransformX(c.hue(), hueSelectorRef.offsetWidth))
-    setColor(c)
-  })
-
-  createEffect(() => {
-    let _sliderSize: Size
-    if (props.sliderSize) {
-      _sliderSize = props.sliderSize
-    } else {
-      _sliderSize = defaultSliderSize
-    }
-    setSliderSize(_sliderSize)
-    //设置滑块边界
-    setSliderMaxX(hueSelectorRef.offsetWidth - _sliderSize.width)
-  })
-
-  const sliderStyles = () => ({
-    width: `${sliderSize().width}px`,
-    height: `${sliderSize().height}px`,
-  })
-
-  function emitChange() {
-    props.onChange?.(color())
-  }
-
-  function onDraggableChange(value: Position) {
-    setColor((c) => c.hue(xTransformHue(value.x, hueSelectorRef.offsetWidth - sliderSize().width)))
-    emitChange()
-  }
-
-  function onSelectHue(e: MouseEvent) {
-    let _slidereX = e.pageX - hueSelectorRef.offsetLeft - sliderSize().width / 2
-    if (_slidereX > sliderMaxX()) {
-      _slidereX = sliderMaxX()
-    }
-    if (_slidereX < 0) {
-      _slidereX = 0
-    }
-    setSliderX(_slidereX)
-    setColor((c) =>
-      c.hue(xTransformHue(_slidereX, hueSelectorRef.offsetWidth - sliderSize().width))
-    )
-    expose.onDrag()
-    emitChange()
-  }
-
-  const expose = {
-    onDrag() {
-      return
+  const defaultProps = mergeProps<Partial<Props>[]>(
+    {
+      color: Color().hsv(),
+      vertical: false,
+      size: { width: 168, height: 12 },
+      sliderSize: { width: 12, height: 12 },
     },
+    props
+  )
+  const [color, setColor] = createSignal(Color())
+  const [sliderPosition, setSliderPosition] = createSignal({ x: 0, y: 0 })
+
+  createEffect(() => {
+    //根据颜色设置滑块位置
+    setSliderPosition(
+      defaultProps.vertical
+        ? {
+            x: 0,
+            y: hueTransformX(
+              defaultProps.color!.hue(),
+              defaultProps.size!.height - defaultProps.sliderSize!.height
+            ),
+          }
+        : {
+            x: hueTransformX(
+              defaultProps.color!.hue(),
+              defaultProps.size!.width - defaultProps.sliderSize!.width
+            ),
+            y: 0,
+          }
+    )
+    setColor(defaultProps.color!)
+  })
+
+  function onRailChange(value: Position) {
+    setColor((c) =>
+      defaultProps.vertical
+        ? c.hue(xTransformHue(value.y, defaultProps.size!.height - defaultProps.sliderSize!.height))
+        : c.hue(xTransformHue(value.x, defaultProps.size!.width - defaultProps.sliderSize!.width))
+    )
+    setSliderPosition(value)
+    defaultProps.onChange?.(color())
   }
+
   return (
-    <div ref={setHueSelectorRef} class="hue-selector" onMouseDown={onSelectHue}>
-      <GDraggable
-        minX={0}
-        maxX={sliderMaxX()}
-        minY={0}
-        maxY={0}
-        x={sliderX()}
-        onChange={onDraggableChange}
-        expose={expose}
-      >
-        <div class="slider" style={sliderStyles()}></div>
-      </GDraggable>
-    </div>
+    <GRail
+      class="g-hue-selector"
+      vertical={props.vertical}
+      size={defaultProps.size}
+      sliderSize={defaultProps.sliderSize}
+      sliderPosition={sliderPosition()}
+      onChange={onRailChange}
+    ></GRail>
   )
 }
