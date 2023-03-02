8032ed232cf0540414602e014e301e8240feee09
lingj <lingj.jhf@outlook.com>
2023-03-02 22:36:15 +0800

    修改导出，添加web-component

---
+++ packages/ui/src/drop-area-group/drop-area-group.tsx
@@ -0,0 +1,141 @@
+import type { JSX } from 'solid-js'
+import { createContext, useContext } from 'solid-js'
+import { customElement } from 'solid-element'
+import { checkCrossEdge, moveItem } from '../utils'
+import { ItemData } from '../drop-area'
+
+interface AreaFunc {
+  get: () => ItemData[]
+  set: (callback: (state: ItemData[]) => void) => void
+}
+
+interface DropAreaGroupProviderValue {
+  addArea: (el: HTMLElement, areaFunc: AreaFunc) => void
+  enterOtherArea: (
+    mousePosition: Position,
+    currentArea: HTMLElement,
+    currentIndex: number,
+    currentItem: ItemData
+  ) => void
+}
+
+export interface GDropAreaGroupProps {
+  children: JSX.Element | JSX.Element[]
+}
+
+const MultiDropAreaContext = createContext<DropAreaGroupProviderValue>()
+
+export const useMultiDropAreaContext = () => useContext(MultiDropAreaContext)
+
+customElement<Partial<GDropAreaGroupProps>>('g-drop-area-group', () => {
+  return (
+    <GDropAreaGroup>
+      <slot></slot>
+    </GDropAreaGroup>
+  )
+})
+
+const GDropAreaGroup = (props: Partial<GDropAreaGroupProps>) => {
+  const areaElementMap = new Map<HTMLElement, AreaFunc>()
+
+  const itemUpadteInfo: {
+    add?: { area: HTMLElement; index: number }
+    remove?: { area: HTMLElement; index: number }
+  } = {}
+
+  function addArea(el: HTMLElement, areaFunc: AreaFunc) {
+    areaElementMap.set(el, areaFunc)
+  }
+
+  function dragEnd() {
+    if (itemUpadteInfo.remove) {
+      areaElementMap.get(itemUpadteInfo.remove.area)?.set((items) => {
+        items.splice(itemUpadteInfo.remove!.index, 1)
+      })
+      itemUpadteInfo.remove = undefined
+    }
+    if (itemUpadteInfo.add) {
+      areaElementMap.get(itemUpadteInfo.add.area)?.set((items) => {
+        items[itemUpadteInfo.add!.index].placeholder = false
+      })
+      itemUpadteInfo.add = undefined
+    }
+    window.removeEventListener('mouseup', dragEnd)
+  }
+
+  function enterOtherArea(
+    mousePosition: Position,
+    currentArea: HTMLElement,
+    currentIndex: number,
+    currentItem: ItemData
+  ) {
+    window.addEventListener('mouseup', dragEnd)
+    let outside = true
+    for (const [otherArea, otherAreaFunc] of areaElementMap.entries()) {
+      if (checkCrossEdge(mousePosition, otherArea.getBoundingClientRect())) {
+        outside = false
+        const areaItems = otherAreaFunc.get()
+        for (const [otherIndex, otherItem] of areaItems.entries()) {
+          if (
+            currentArea === otherArea &&
+            otherIndex !== currentIndex &&
+            checkCrossEdge(mousePosition, otherItem.el!.getBoundingClientRect())
+          ) {
+            otherAreaFunc.set((items) => {
+              moveItem(currentIndex, otherIndex, items)
+            })
+            return
+          }
+          if (
+            currentArea !== otherArea &&
+            checkCrossEdge(mousePosition, otherItem.el!.getBoundingClientRect())
+          ) {
+            if (!itemUpadteInfo.add) {
+              otherAreaFunc.set((items) => {
+                items.splice(otherIndex, 0, currentItem)
+                items[otherIndex].placeholder = true
+              })
+            } else {
+              otherAreaFunc.set((items) => {
+                moveItem(itemUpadteInfo.add!.index, otherIndex, items)
+              })
+            }
+            itemUpadteInfo.remove = { area: currentArea, index: currentIndex }
+            itemUpadteInfo.add = { area: otherArea, index: otherIndex }
+            return
+          }
+        }
+      }
+    }
+    if (outside) {
+      if (itemUpadteInfo.add) {
+        areaElementMap.get(itemUpadteInfo.add.area)?.set((items) => {
+          items.splice(itemUpadteInfo.add!.index, 1)
+        })
+        itemUpadteInfo.add = undefined
+      }
+      if (itemUpadteInfo.remove) {
+        itemUpadteInfo.remove = undefined
+      }
+    }
+  }
+
+  const providerValue: DropAreaGroupProviderValue = {
+    addArea,
+    enterOtherArea,
+  }
+
+  return (
+    <MultiDropAreaContext.Provider value={providerValue}>
+      {props.children}
+    </MultiDropAreaContext.Provider>
+  )
+}
+
+declare module 'solid-js' {
+  namespace JSX {
+    interface IntrinsicElements {
+      'g-drop-area-group': Partial<GDropAreaGroupProps>
+    }
+  }
+}
