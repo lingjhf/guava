c1d4717f65818373a557d05365af85a63398559d
lingj <lingj.jhf@outlook.com>
2023-03-02 22:39:45 +0800

    修改打包配置

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,16 +1,8 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
-import Unocss from 'unocss/vite'
-import { presetWind, transformerDirectives } from 'unocss'
 import autoprefixer from 'autoprefixer'
 export default defineConfig({
-  plugins: [
-    solidPlugin(),
-    Unocss({
-      presets: [presetWind({ preflight: false })],
-      transformers: [transformerDirectives()],
-    }),
-  ],
+  plugins: [solidPlugin()],
   css: {
     postcss: {
       plugins: [autoprefixer({ overrideBrowserslist: [] })],
@@ -22,19 +14,15 @@ export default defineConfig({
   build: {
     target: 'esnext',
     outDir: 'dist/es',
-    cssCodeSplit: true,
     lib: {
       entry: './src/index.ts',
       formats: ['es'],
     },
     rollupOptions: {
-      external: ['solid-js', 'solid-js/web', 'solid-js/store', 'color'],
+      external: ['solid-js', 'solid-js/web', 'solid-js/store', 'solid-element', 'color'],
       output: {
         entryFileNames(chunkInfo) {
           return `${chunkInfo.name}.js`
-        },
-        assetFileNames(chunkInfo) {
-          return `${chunkInfo.name}`
         },
         chunkFileNames(chunkInfo) {
           return `${chunkInfo.name}.js`
