37f6c056a2bc24ed1408822f79074d444551beed
lingj <lingj.jhf@outlook.com>
2023-03-02 22:36:30 +0800

    添加辅助函数

--- packages/ui/src/utils/index.ts
+++ packages/ui/src/utils/index.ts
@@ -1,5 +1,3 @@
-export * from './type'
-
 class PressedDrag {
   events = new Map()
 
@@ -55,4 +53,58 @@ class PressedDrag {
 
 export function createPressedDrag() {
   return new PressedDrag()
+}
+
+export function moveItem(sourceIndex: number, targetIndex: number, arr: unknown[]) {
+  const itemTemp = arr[sourceIndex]
+  arr.splice(sourceIndex, 1)
+  arr.splice(targetIndex, 0, itemTemp)
+}
+
+export function checkCrossEdge(source: Position, target: SizePosition): boolean {
+  if (
+    source.x > target.x &&
+    source.x < target.x + target.width &&
+    source.y > target.y &&
+    source.y < target.y + target.height
+  ) {
+    return true
+  }
+  return false
+}
+
+/**
+ *
+ * @param source
+ * @param target
+ * @returns 1表示在上面，0表示在下面，-1表示不在范围
+ */
+export function checkCrossHeightHalf(source: Position, target: SizePosition): number {
+  if (source.x > target.x && source.x < target.x + target.width) {
+    if (source.y > target.y && source.y < target.y + target.height / 2) {
+      return 1
+    }
+    if (source.y > target.y && source.y < target.y + target.height) {
+      return 0
+    }
+  }
+  return -1
+}
+
+/**
+ *
+ * @param source
+ * @param target
+ * @returns 1表示在上面，0表示在下面，-1表示不在范围
+ */
+export function checkCrossWidthHalf(source: Position, target: SizePosition): number {
+  if (source.y > target.y && source.y < target.y + target.height) {
+    if (source.x > target.x && source.x < target.x + target.width / 2) {
+      return 1
+    }
+    if (source.x > target.x && source.x < target.x + target.width) {
+      return 0
+    }
+  }
+  return -1
 }
