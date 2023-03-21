e7e49c34f95de72395adc94b2e79cd28829db471
lingj <lingj.jhf@outlook.com>
2023-03-21 23:32:23 +0800

    修改虚拟滚动

--- packages/vue/src/virtual-scroll.ts
+++ packages/vue/src/virtual-scroll.ts
@@ -2,37 +2,31 @@ import type { PropType } from 'vue'
 import type { GVirtualScrollProps } from '@lingjhf/guava'
 import { defineComponent, h } from 'vue'
 import '@lingjhf/guava/lib/virtual-scroll'
-import { slotToDom } from './utils'
 
 export default defineComponent({
   props: {
-    items: Array as PropType<GVirtualScrollProps['items']>,
     horizontal: Boolean as PropType<GVirtualScrollProps['horizontal']>,
-    buffer: Number as PropType<GVirtualScrollProps['buffer']>,
-    change: Function as PropType<GVirtualScrollProps['change']>,
+    scrollChange: Function as PropType<GVirtualScrollProps['scrollChange']>,
   },
-  setup(props, { slots }) {
+  emits: ['scrollChange'],
+  setup(props, { slots, emit }) {
+    function scrollChange(value: number) {
+      emit('scrollChange', value)
+    }
     return () =>
-      h('g-virtual-scroll', {
-        horizontal: props.horizontal,
-        buffer: props.buffer,
-        ref(ref: unknown) {
-          const dom = ref as GVirtualScrollProps
-          if (!dom) return
-          dom.items = props.items ?? []
-          if (slots.renderItem) {
-            dom.renderItem = (key: string) => {
-              let renderEl: HTMLElement | null = null
-              slotToDom(slots, 'renderItem', { key }, (el) => {
-                renderEl = el
-              })
-              return renderEl
-            }
-          }
-          if (props.change) {
-            dom.change = props.change
-          }
+      h(
+        'g-virtual-scroll',
+        {
+          horizontal: props.horizontal,
+          ref(ref: unknown) {
+            const dom = ref as GVirtualScrollProps
+            if (!dom) return
+            dom.scrollChange = scrollChange
+          },
         },
-      })
+        {
+          default: () => (slots.default ? slots.default() : null),
+        }
+      )
   },
 })
