93694c76b235f0a9520b8b4206e5f82366bae3af
lingj <lingj.jhf@outlook.com>
2023-06-12 22:28:46 +0800

    开发代码

--- packages/core/src/pagination/pagination.tsx
+++ packages/core/src/pagination/pagination.tsx
@@ -1,3 +1,17 @@
+import { ComponentProps } from '../types'
+
+export interface PaginationProps extends ComponentProps<HTMLDivElement> {
+  //当前页码
+  currentPage: number
+  //总数
+  total: number
+  //每页数量
+  pageSize: number
+  //最大页码按钮数量
+  maxPage: number
+  disabled: boolean
+}
+
 export const Pagination = () => {
 
   return (
