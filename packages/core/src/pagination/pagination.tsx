f68616aa348a27abfabb932b8f07d53796334b26
JHFeng <lingj.jhf@outlook.com>
2023-06-14 16:13:39 +0800

    开发代码

--- packages/core/src/pagination/pagination.tsx
+++ packages/core/src/pagination/pagination.tsx
@@ -1,13 +1,15 @@
-import { For, createEffect, createSignal } from 'solid-js'
+import { For, createEffect, createSignal, on } from 'solid-js'
 import { ComponentProps } from '../types'
 import { customEventHandlersName, generateProps } from '../utils'
 import { MoreFilled } from '../icon/more-filled'
+import { ChevronLeftFilled } from '../icon/chevron-left-filled'
+import { ChevronRightFilled } from '../icon/chevron-right-filled'
 import styles from './pagination.module.css'
 export interface PaginationProps extends ComponentProps<HTMLDivElement> {
   currentPage: number
   pageSize: number
   total: number
-  maxPager: number
+  maxPager: 5 | 7 | 9 | 11 | 13 | 15 | 17 | 19 | 21
 }
 
 interface Pager {
@@ -31,15 +33,22 @@ export const Pagination = (propsRaw: Partial<PaginationProps>) => {
   const [pagers, setPagers] = createSignal<Pager[]>([])
   const [currentPage, setCurrentPage] = createSignal(1)
   let totalPage = 0
+  let maxPager = 1
 
   createEffect(() => {
     if (props.total <= 0) {
       return
     }
     totalPage = Math.ceil(props.total / props.pageSize)
+    if (props.maxPager > totalPage) {
+      maxPager = totalPage
+    }
     setCurrentPage(props.currentPage)
-    setPagers(generatePagers(props.currentPage, props.maxPager, totalPage))
   })
+
+  createEffect(on(currentPage, (v) => {
+    setPagers(generatePagers(v, maxPager, totalPage))
+  }))
 
   const paginationItemClasses = (page: number) => {
     let classes = `${styles.paginationItem}`
@@ -51,64 +60,78 @@ export const Pagination = (propsRaw: Partial<PaginationProps>) => {
 
   function generatePagers(currentPage: number, maxPager: number, totalPage: number): Pager[] {
     const pagers: Pager[] = [{ page: 1 }]
-    const a = Math.floor((maxPager - 2) / 2)
-    if (currentPage + a < maxPager) {
+    const half = Math.floor((maxPager - 2) / 2)
+    if (currentPage + half < maxPager) {
       for (let i = 2; i < maxPager; i++) {
         pagers.push({ page: i })
       }
       pagers.push({ isQuickNext: true })
-    } else if (currentPage - a > totalPage - (maxPager - 1)) {
+    } else if (currentPage - half > totalPage - (maxPager - 1)) {
       pagers.push({ isQuickPrev: true })
       for (let i = totalPage - (maxPager - 2); i < totalPage; i++) {
         pagers.push({ page: i })
       }
     } else {
       pagers.push({ isQuickPrev: true })
-      for (let i = currentPage - 2; i <= currentPage + 2; i++) {
+      for (let i = currentPage - half; i <= currentPage + half; i++) {
         pagers.push({ page: i })
       }
       pagers.push({ isQuickNext: true })
     }
     pagers.push({ page: totalPage })
+    console.log(pagers)
     return pagers
   }
 
-  function quickPrev() {
-    const value = setCurrentPage(v => {
-      let value = v - (props.maxPager - 2)
+  function prev() {
+    setCurrentPage(v => {
+      let value = v - 1
       if (value < 1) {
         value = 1
       }
       return value
     })
-    setPagers(generatePagers(value, props.maxPager, totalPage))
   }
 
-  function quickNext() {
-    const value = setCurrentPage(v => {
-      let value = v + (props.maxPager - 2)
+  function next() {
+    setCurrentPage(v => {
+      let value = v + 1
       if (value > totalPage) {
         value = totalPage
       }
       return value
     })
-    setPagers(generatePagers(value, props.maxPager, totalPage))
+  }
+
+  function quickPrev() {
+    setCurrentPage(v => {
+      let value = v - (maxPager - 2)
+      if (value < 1) {
+        value = 1
+      }
+      return value
+    })
+  }
+
+  function quickNext() {
+    setCurrentPage(v => {
+      let value = v + (maxPager - 2)
+      if (value > totalPage) {
+        value = totalPage
+      }
+      return value
+    })
   }
 
   function currentPageChange(value: number) {
     setCurrentPage(value)
-    setPagers(generatePagers(value, props.maxPager, totalPage))
-  }
-
-  const PaginationDefault = () => {
-    return (
-      <div></div>
-    )
   }
 
   return (
     <div class={styles.pagination} {...eventHandlers}>
-      
+      <div class={styles.paginationPrev} onClick={prev}>
+        <ChevronLeftFilled />
+      </div>
       <For each={pagers()}>
         {
           (item) => {
@@ -124,6 +147,9 @@ export const Pagination = (propsRaw: Partial<PaginationProps>) => {
           }
         }
       </For>
+      <div class={styles.paginationNext} onClick={next}>
+        <ChevronRightFilled />
+      </div>
     </div>
   )
 }
--- packages/dev/src/App.tsx
+++ packages/dev/src/App.tsx
@@ -10,7 +10,7 @@ const App: Component = () => {
     <div class=' h-screen bg-#1A1A1A w-screen'>
       <GConfigProvider dark>
         <div class='p-2 w-300px'>
-          <GPagination total={100}></GPagination>
+          <GPagination total={30} maxPager={11}></GPagination>
         </div>
       </GConfigProvider>
     </div>
