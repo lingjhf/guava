c8aa989efe921a2e484d8213b31758f6d8e6d15d
JHFeng <lingj.jhf@outlook.com>
2023-06-15 14:05:22 +0800

    开发代码

--- packages/core/src/switch/switch.tsx
+++ packages/core/src/switch/switch.tsx
@@ -1,7 +1,73 @@
+import { createSignal } from 'solid-js'
+import { ComponentProps } from '../types'
+import styles from './switch.module.css'
+import { customEventHandlersName, generateProps } from '../utils'
 
-export const Switch = () => {
+export type SwitchType = 'default' | 'small' | 'large'
+export interface SwitchProps extends ComponentProps<HTMLDivElement> {
+  off: boolean
+  size: SwitchType,
+  loading: boolean | (() => Promise<unknown>)
+  disabled: boolean
+}
+
+const sizeClasses = {
+  default: styles.switchDefaultSize,
+  small: styles.switchSmall,
+  large: styles.switchLarge,
+}
+
+const actionSizeClasses = {
+  default: styles.switchActionDefaultSize,
+  small: styles.switchActionSmall,
+  large: styles.switchActionLarge,
+}
+
+export const Switch = (propsRaw: Partial<SwitchProps>) => {
+
+  const [eventHandlers, props] = generateProps(
+    propsRaw,
+    {
+      off: true,
+      size: 'default',
+      loading: false,
+      disabled: false
+    },
+    customEventHandlersName,
+  )
+
+  const [off, setOffset] = createSignal(true)
+
+  const switchClasses = () => {
+    let classes = ` ${styles.switch} ${sizeClasses[props.size]}`
+    if (off()) {
+      classes += ` ${styles.switchOff}`
+    } else {
+      classes += ` ${styles.switchOn}`
+    }
+    return classes
+  }
+
+  const switchActionClasses = () => {
+    let classes = ` ${styles.switchAction} ${actionSizeClasses[props.size]}`
+    if (off()) {
+      classes += ` ${styles.switchActionOff}`
+    } else {
+      classes += ` ${styles.switchActionOn}`
+    }
+    return classes
+  }
+
+  async function switchChange() {
+    if (typeof props.loading === 'function') {
+      await props.loading()
+    }
+    setOffset(v => !v)
+  }
 
   return (
-    <div></div>
+    <div class={switchClasses()} {...eventHandlers} onClick={switchChange} >
+      <div class={switchActionClasses()}></div>
+    </div>
   )
 }
--- packages/dev/src/App.tsx
+++ packages/dev/src/App.tsx
@@ -1,20 +1,21 @@
 import type { Component } from 'solid-js'
-import { GConfigProvider, GInput, GPagination } from '@lingjhf/guava'
+import { GConfigProvider, GSwitch } from '@lingjhf/guava'
 import 'virtual:uno.css'
 
 const App: Component = () => {
-  function ok(v: string) {
-    console.log(v)
+  async function ok() {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('ok')
+        resolve(0)
+      }, 3000)
+    })
   }
   return (
     <div class=' h-screen bg-#1A1A1A w-screen'>
       <GConfigProvider dark>
         <div class='p-2 w-300px'>
-          <GPagination total={100} size={48} prev={<span>prev</span>} next={<span>next</span>}
-            quickNext={<span>qn</span>}
-            quickPrev={<span>qp</span>}
-
-          ></GPagination>
+          <GSwitch size='large' loading={ok}></GSwitch>
         </div>
       </GConfigProvider>
     </div>
