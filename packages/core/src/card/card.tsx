f0b3e3702ef2b4bd1d6fe635e2c444d8983bfa73
JHFeng <lingj.jhf@outlook.com>
2023-06-20 10:27:05 +0800

    开发代码

--- packages/core/src/card/card.tsx
+++ packages/core/src/card/card.tsx
@@ -1,7 +1,21 @@
+import { Show, JSX } from 'solid-js'
+import { ComponentPropsWithChildren } from '../types'
+import { customEventHandlersName, generateProps } from '../utils'
+import styles from './card.module.css'
 
-export const Card = () => {
+export interface CardProps extends ComponentPropsWithChildren<HTMLDivElement> {
+  header?: JSX.Element
+  footer?: JSX.Element
+}
 
+export const Card = (propsRaw: Partial<CardProps>) => {
+  const [eventHandlers, props] = generateProps(propsRaw, {}, customEventHandlersName)
   return (
-    <div></div>
+    <div class={styles.card} {...eventHandlers}>
+      <Show when={props.header}>
+        <div class={styles.cardHeader}>{props.header}</div>
+      </Show>
+      <div class={styles.cardContent}>{props.children}</div>
+    </div>
   )
 }
