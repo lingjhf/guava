6ffb4607b7a51fe2adb5fdb663222d7e344c7098
JHFeng <lingj.jhf@outlook.com>
2023-07-08 18:02:30 +0800

    开发代码

--- packages/dev/src/App.tsx
+++ packages/dev/src/App.tsx
@@ -1,5 +1,5 @@
 
-import { GConfigProvider, GTabs, GTab } from '@lingjhf/guava'
+import { GConfigProvider, GTabs, GTab, GScrollbar } from '@lingjhf/guava'
 
 import 'virtual:uno.css'
 const App = () => {
@@ -8,13 +8,11 @@ const App = () => {
     <div class=' h-screen bg-#1A1A1A w-screen'>
       <GConfigProvider dark>
         <div class=' px-100px'>
-          <GTabs size='large'>
-            <GTab >One</GTab>
-            <GTab>Tow</GTab>
-            <GTab>Three</GTab>
-            <GTab>Four</GTab>
-            <GTab>Five</GTab>
-          </GTabs>
+          <div class=' w-300px h-300px '>
+            <GScrollbar>
+              <div class=' w-3000px h-3000px'></div>
+            </GScrollbar>
+          </div>
         </div>
       </GConfigProvider>
     </div>
