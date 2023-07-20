72eb446a538b37a492c668660a92a3b32a2426de
JHFeng <lingj.jhf@outlook.com>
2023-07-20 18:16:54 +0800

    文档

---
+++ packages/docs/src/example/collapse/collapse-base-usage.tsx
@@ -0,0 +1,34 @@
+import { CodeExample } from '../../components/code-example'
+import { GCollapse, GCollapseItem } from '@lingjhf/guava'
+
+const code = `
+
+`
+export const CollapseBaseUsage = () => {
+
+  return (
+    <CodeExample code={code} language='jsx'>
+      <GCollapse>
+        <GCollapseItem header={'Collapse item1'}>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+        </GCollapseItem>
+        <GCollapseItem header={'Collapse item1'}>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+        </GCollapseItem>
+        <GCollapseItem header={'Collapse item1'}>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+          <p>Collapse content</p>
+        </GCollapseItem>
+      </GCollapse>
+    </CodeExample>
+  )
+}
+
