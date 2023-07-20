8d59ccbc5316c255062198fd7221223aebaa7496
JHFeng <lingj.jhf@outlook.com>
2023-07-20 16:27:02 +0800

    开发代码

--- packages/dev/src/App.tsx
+++ packages/dev/src/App.tsx
@@ -1,33 +1,23 @@
-
-import { GConfigProvider, GDrawer, GButton, GBreadcrumb, GBreadcrumbItem } from '@lingjhf/guava'
+import { GConfigProvider, GVirtualScroll, GVirtualList } from '@lingjhf/guava'
 import { For, createSignal } from 'solid-js'
 
 import 'virtual:uno.css'
 const App = () => {
-  const [items, setItems] = createSignal(['Breadcrumb node1', 'Breadcrumb node2', 'Breadcrumb node3', 'Breadcrumb node4'])
-  function change() {
-    setItems(v => {
-      const n = [...v]
-      n[2] = 'xxxx'
-      return n
-    })
-  }
+  const items = Array.from({ length: 10000 }).map((_, index) => ({ key: `${index + 1}`, height: 40 }))
+  console.log(items.length)
   return (
     <div class=' h-screen bg-#1A1A1A w-screen'>
       <GConfigProvider dark>
         <div class=' px-100px'>
-          <GButton onClick={change}>change</GButton>
-          <GBreadcrumb size='large'>
-            <For each={items()}>
-              {
-                (item) => {
-                  return (
-                    <GBreadcrumbItem>{item}</GBreadcrumbItem>
-                  )
+          <div class='h-300px'>
+            <GVirtualScroll>
+              <GVirtualList items={items}>{
+                (key) => {
+                  return <div class='bg-[var(--bg-brand-light-default)] h-full box-border h-32px  flex items-center justify-center'>{key}</div>
                 }
-              }
-            </For>
-          </GBreadcrumb>
+              }</GVirtualList>
+            </GVirtualScroll>
+          </div>
         </div>
       </GConfigProvider>
     </div>
