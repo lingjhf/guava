37b44f1fd85994b9770b49282ad5bf3115e6121d
JHFeng <lingj.jhf@outlook.com>
2023-07-20 16:39:01 +0800

    文档

---
+++ packages/docs/src/example/virtual-scroll/virtual-scroll-base-usage.tsx
@@ -0,0 +1,21 @@
+import { CodeExample } from '../../components/code-example'
+import { GVirtualScroll, GVirtualList } from '@lingjhf/guava'
+
+export const VirtualScrollBaseUsage = () => {
+  const code = `
+  `
+  const items = Array.from({ length: 100000 }).map((_, index) => ({ key: `${index + 1}`, height: 40 }))
+  return (
+    <CodeExample code={code} language='jsx'>
+      <div class='h-300px'>
+        <GVirtualScroll>
+          <GVirtualList items={items}>{
+            (key) => {
+              return <div class='bg-[var(--bg-brand-light-default)] h-32px flex items-center justify-center'>{key}</div>
+            }
+          }</GVirtualList>
+        </GVirtualScroll>
+      </div>
+    </CodeExample>
+  )
+}
---
+++ packages/docs/src/example/virtual-scroll/virtual-scroll-horizontal-usage.tsx
@@ -0,0 +1,22 @@
+import { CodeExample } from '../../components/code-example'
+import { GVirtualScroll, GVirtualList } from '@lingjhf/guava'
+
+export const VirtualScrollHorizontalUsage = () => {
+  const code = `
+  `
+  const items = Array.from({ length: 100000 }).map((_, index) => ({ key: `${index + 1}`, height: 80 }))
+  return (
+    <CodeExample code={code} language='jsx'>
+
+      <GVirtualScroll horizontal>
+        <div class='h-40px'>
+          <GVirtualList items={items}>{
+            (key) => {
+              return <div class='bg-[var(--bg-brand-light-default)] h-32px w-72px flex items-center justify-center'>{key}</div>
+            }
+          }</GVirtualList>
+        </div>
+      </GVirtualScroll>
+    </CodeExample>
+  )
+}
