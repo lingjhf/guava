4189e7a17189b1f3fb6b23d0c6bb058e1ab13bf8
lingj <lingj.jhf@outlook.com>
2023-07-23 02:46:16 +0800

    开发代码

--- packages/docs/astro.config.ts
+++ packages/docs/astro.config.ts
@@ -2,12 +2,16 @@ import { defineConfig } from 'astro/config'
 import solidJs from '@astrojs/solid-js'
 import UnoCSS from 'unocss/astro'
 import mdx from '@astrojs/mdx'
+import { transformerDirectives } from 'unocss'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     solidJs(),
     mdx(),
-    UnoCSS({ extraContent: { filesystem: ['src/example/**/*'] } }),
+    UnoCSS({
+      extraContent: { filesystem: ['src/example/**/*'] },
+      transformers: [transformerDirectives()]
+    }),
   ],
 })
