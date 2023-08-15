29d36c48524710e77e3bcbd7b35bb0b8860472b9
JHFeng <lingj.jhf@outlook.com>
2023-08-15 18:40:03 +0800

    开发代码

--- packages/core/src/date/day-container.tsx
+++ packages/core/src/date/day-container.tsx
@@ -1,9 +1,24 @@
+import type { GuavaProps } from '../types'
 import { HourLine } from './hour-line'
+import { For, createMemo, type JSX } from 'solid-js'
+import styles from './day-container.module.css'
+import { generateSplitEventHandlersProps } from '../utils'
 
+export interface DayContainerProps extends GuavaProps<HTMLDivElement> {
+  renderHour?: (hour: number) => JSX.Element
+  renderRow?: (hour: number) => JSX.Element
+}
 
-export const DayContainer = () => {
+export const DayContainer = (propsRaw: Partial<DayContainerProps>) => {
+  const [eventHandlers, props] = generateSplitEventHandlersProps(
+    propsRaw,
+    {}
+  )
 
   return (
-    <div></div>
+    <HourLine
+      renderHour={(hour) => props.renderHour?.(hour)}
+      renderRow={(hour) => props.renderRow?.(hour)}
+    />
   )
 }
